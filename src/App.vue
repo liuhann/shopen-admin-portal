851c351f7176eb58c22c3184fc8a6c4143788bac
liuhan <liuhan@xbniao.com>
2018-03-19 13:13:46 +0800

    cleaner code

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
+<style lang="less">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
+
+
 <template>
   <div id="app">
     <div class="main-frame" v-if="frame==='main'">
       <img src="./assets/logo.png">
       <router-view/>
     </div>
-
     <div class="main-frame" v-if="frame==='login'">
       <img src="./assets/logo.png">
       <router-view/>
     </div>
-
     <div class="main-frame" v-if="frame==='open'">
       <img src="./assets/logo.png">
       <router-view/>
     </div>
-
   </div>
 </template>
 
@@ -22,18 +31,9 @@
   export default {
     name: 'App',
     data() {
-      frame: 'main'
+      return {
+        frame: 'main'
+      }
     },
   };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
