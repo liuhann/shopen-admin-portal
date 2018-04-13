83b8036f8ec236b6dae69430ed64cb4df028b44f
liuhan <liuhan@xbniao.com>
2018-04-13 17:49:58 +0800

    basic boot

---
+++ src/modules/site/pages/create/mobile-shell-nova.vue
@@ -0,0 +1,63 @@
+<style lang="less">
+.mobile-shell-nova-outer {
+  width: 80px;
+  height: 160px;
+  border: 1px solid #c7c7c7;
+  border-radius: 10px;
+  position: relative;
+  transition: transform .3s linear;
+  .screen {
+    width: 76px;
+    left: 1px;
+    top: 6px;
+    position: absolute;
+    height: 140px;
+    border-radius: 5px;
+    border: 1px solid #c7c7c7;
+  }
+  .home-btn {
+    position: absolute;
+    width: 20px;
+    left: 30px;
+    bottom: 3px;
+    height: 4px;
+    border-radius: 5px;
+    border: 1px solid #c7c7c7;
+  }
+}
+</style>
+
+<template>
+<div class="mobile-shell-nova-outer" :style="checked? check: {}">
+  <div class="screen" :style="background">
+  </div>
+  <div class="home-btn">
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'mobile-shell-nova',
+  props: {
+    checked: {
+      type: Boolean
+    },
+    cover: {
+      type: String
+    }
+  },
+  data () {
+    return {
+      check: {
+        transform: 'scale(1.1)',
+        border: '1px solid #ff8401'
+      },
+      background: {
+        backgroundImage: 'url("' + this.cover + '")',
+        backgroundSize: 'cover'
+      }
+    }
+  }
+}
+</script>
--- src/modules/site/pages/create/page-site-create.vue
+++ src/modules/site/pages/create/page-site-create.vue
@@ -1,28 +1,148 @@
 <style lang="less">
   @import '../../style/common';
+  .site-create {
+    .themes {
+      display: flex;
+      justify-content: space-around;
+      height: 200px;
+      align-items: center;
+
+      .mobile-shell-nova-outer {
+        &:hover {
+          transform: scale(1.1);
+          cursor: pointer;
+        }
+        &.selected {
+          border: 1px solid red;
+        }
+      }
+    }
+    .packages {
+      display: flex;
+      flex-wrap: wrap;
+      .package {
+        display: flex;
+        text-align: center;
+        width: 100px;
+        flex-direction: column;
+        border: 1px solid #eee;
+        height: 100px;
+        margin: 20px;
+        i {
+          font-size: 30px;
+          margin: 20px;
+          margin-bottom: 5px;
+          color: #273345;
+        }
+      }
+    }
+  }
 </style>
 
 <template>
-<div class="site-layout">
+<div class="site-layout site-create">
   <div class="header">
     <div class="header-actions">
-      <el-button type="primary" size="medium" plain icon="el-icon-edit" @click="returnList">返回列表</el-button>
+      <el-button type="text" size="medium" icon="el-icon-back" @click="returnList">返回列表</el-button>
+      <el-button type="text" size="medium" icon="el-icon-check" @click="returnList">保存</el-button>
     </div>
   </div>
   <div class="content">
+    <section class="form">
+      <label>
+          <span class="key">名称</span>
+          <span class="val">
+            <input type="text" v-model="site.name" placeholder="站点名称">
+          </span>
+      </label>
+      <label>
+        <span class="key">主题</span>
+        <span class="val themes">
+          <mobile-shell-nova :checked="item.id===site.theme" @click.native="chooseTheme(item)" v-for="(item, index) in themes" :key="index" :cover="getCoverImage(item)"></mobile-shell-nova>
+        </span>
+      </label>
+      <label>
+        <span class="key">功能模块</span>
+        <span class="val packages">
+          <div class="package" v-for="module in packages" :key="module.name" @click="togglePackage(module)" :class="module.selected?'checked': ''">
+            <i :class="module.icon"></i>
+            <div class="name">{{module.name}}</div>
+          </div>
+        </span>
+      </label>
+    </section>
   </div>
 </div>
 </template>
 
 <script>
 import {Button} from 'element-ui'
+import MobileShellNova from './mobile-shell-nova'
 
 export default {
   name: 'page-site-create',
   components: {
-    'el-button': Button
+    MobileShellNova,
+    'el-button': Button,
+    'mobile-shell-nova': MobileShellNova
   },
+  // http://localhost:3000/themes/bonfire/previews/cover.png
+  data () {
+    return {
+      site: {
+        name: '',
+        theme: 'bonfire',
+        packages: []
+      },
+      themes: [{
+        id: 'bonfire'
+      }, {
+        id: 'dasha'
+      }, {
+        id: 'indochino'
+      }, {
+        id: 'bonfire'
+      }],
+      packages: [{
+        selected: false,
+        id: 'store',
+        name: '商店',
+        icon: 'el-icon-goods'
+      }, {
+        selected: false,
+        id: 'story',
+        name: '故事',
+        icon: 'el-icon-service'
+      }, {
+        selected: false,
+        name: '视频',
+        icon: 'el-icon-picture'
+      }, {
+        selected: false,
+        name: '微信公号',
+        icon: 'el-icon-view'
+      }, {
+        selected: false,
+        id: 'activity',
+        name: '活动',
+        icon: 'el-icon-date'
+      }]
+    }
+  },
+
   methods: {
+    getCoverImage (theme) {
+      return this.ctx.servers.image.options.baseURL + '/themes/' + theme.id + '/previews/cover.png'
+    },
+
+    chooseTheme (theme) {
+      this.site.theme = theme.id
+    },
+
+    togglePackage (module) {
+      module.selected = !module.selected
+    },
+
     returnList () {
       this.$router.replace('/sites')
     }
